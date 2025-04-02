5771c742d418cd198cf549b0fdd8c99b838ba3ae
itsSidshub <nayaksidharthakumar1@gmail.com>
2025-04-02 17:47:28 +0530

    my assignment

---
+++ ques6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void printPattern(int n) {
+    // Loop for each line (1 to n)
+    for (int i = 1; i <= n; i++) {
+        // For each line, each digit 1 to n is repeated i times
+        for (int digit = 1; digit <= n; digit++) {
+            // Repeat the current digit i times
+            for (int repeat = 1; repeat <= i; repeat++) {
+                cout << digit;
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    
+    cout << "Output for n = " << n << ":" << endl;
+    printPattern(n);
+    
+    return 0;
+}
